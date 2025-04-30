fbb544d49232159c5283eb6a3fa362760ee47198
Wils96 <wilsonsarah0207@gmail.com>
2025-04-30 17:28:18 -0400

    Update ui.py

--- basketball/app/game/ui.py
+++ basketball/app/game/ui.py
@@ -1,10 +1,6 @@
-git clone https://github.com/<you>/<repo>.git
-cd repo
-git pull
-
-git rm path/to/the/file.ext
-
-git commit -m "Remove file.ext"
-
-git push origin main
-# (or the branch you’re working on)
+git clone https://github.com/Wils96/Basketball_Cog.git
+cd Basketball_Cog
+git checkout -b delete-file-branch
+git rm basketball/app/game/ui.py
+git commit -m "Delete basketball/app/game/ui.py"
+git push origin delete-file-branch
