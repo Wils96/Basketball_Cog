72a8b4a2a83c2719a1146d028f706e6c26a71401
Wils96 <wilsonsarah0207@gmail.com>
2025-04-30 16:56:27 -0400

    Update ui.py

--- basketball/app/game/ui.py
+++ basketball/app/game/ui.py
@@ -1,14 +1,14 @@
-# 1. Clone or update
+
 git clone https://github.com/<you>/<repo>.git
 cd repo
 git pull
 
-# 2. Remove the file
+
 git rm path/to/the/file.ext
 
-# 3. Commit the deletion
+
 git commit -m "Remove file.ext"
 
-# 4. Push back up
+
 git push origin main
 # (or the branch you’re working on)
